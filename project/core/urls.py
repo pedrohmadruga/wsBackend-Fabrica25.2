934cdc9aa043e8bf690519623e4dba769c409ba7
Pedro Madruga <pedrohfonseca01@gmail.com>
2025-09-13 12:01:52 -0300

    feat :arrow_up: Criando página para visualizar os livros

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, BookSearchView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name = 'home')
+    path('', HomeView.as_view(), name = 'home'),
+    path('search/', BookSearchView.as_view(), name = 'search')
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,4 +1,35 @@
 from django.views.generic import TemplateView
+import requests
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class BookSearchView(TemplateView):
+    template_name = "search.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get("q", "")
+        results = []
+
+        if query:
+            url = "https://www.googleapis.com/books/v1/volumes"
+            params = {
+                "q": query,
+                "maxResults": 20,
+            }
+            response = requests.get(url, params=params)
+            if response.status_code == 200:
+                data = response.json()
+                for item in data.get("items", []):
+                    info = item.get("volumeInfo", {})
+                    results.append({
+                        "title": info.get("title", "Sem título"),
+                        "authors": ", ".join(info.get("authors") or ["Desconhecido"]),
+                        "publisher": info.get("publisher", "Desconhecido"),
+                        "published_date": info.get("publishedDate", "Desconhecido"),
+                        "thumbnail": (info.get("imageLinks") or {}).get("thumbnail"),
+                    })
+        context["results"] = results
+        context["query"] = query
+        return context
